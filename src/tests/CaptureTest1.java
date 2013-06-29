4451a4ab336284bd11ef058cd0aebf70f4d2a15e
davidofthedragons <davidofthedragons@gmail.com>
2013-06-29 10:24:20 -0400

    Now uses 2 cameras

--- src/tests/CaptureTest1.java
+++ src/tests/CaptureTest1.java
@@ -8,6 +8,8 @@ import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
 import org.opencv.core.*;
 import org.opencv.highgui.Highgui;
@@ -16,21 +18,47 @@ import org.opencv.highgui.VideoCapture;
 public class CaptureTest1 extends JFrame {
 	
 	VideoCapture cap;
-	BufferedImage img;
+	VideoCapture capUSB;
+	BufferedImage imgr;
+	BufferedImage imgl;
 	int fps = 50;
+	
+	JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+	JPanel right = new JPanel() {
+		public void paint(Graphics g) {
+			g.drawImage(imgr, 0, 0, null);
+		}
+	};
+	JPanel left = new JPanel() {
+		public void paint(Graphics g) {
+			g.drawImage(imgl, 0, 0, null);
+		}
+	};
 	
 	public CaptureTest1() {
 		super("Capture Test 1");
-		setSize(648, 488);
+		setSize(648*2, 488);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		cap = new VideoCapture(0);
-		if(!cap.isOpened()) {
+		cap = new VideoCapture(1);
+		capUSB = new VideoCapture(0);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		if(!cap.isOpened() || !capUSB.isOpened()) {
 			System.out.println("Could not find camera");
 			System.exit(1);
 		}
 		System.out.println("Found camera " + cap.toString());
+		
+		splitPane.setRightComponent(right);
+		splitPane.setLeftComponent(left);
+		add(splitPane);
+		
 		
 		Thread capture = new Thread(new Runnable() {
 			public void run() {
@@ -50,10 +78,6 @@ public class CaptureTest1 extends JFrame {
 		setVisible(true);
 	}
 	
-	@Override
-	public void paint(Graphics g) {
-		g.drawImage(img, 0, 0, null);
-	}
 	
 	public void loop() {
 		Mat frame = new Mat();
@@ -62,11 +86,26 @@ public class CaptureTest1 extends JFrame {
 		MatOfByte tempMat = new MatOfByte();
 		Highgui.imencode(".jpg", frame, tempMat);
 		byte[] byteArray = tempMat.toArray();
-		img = null;
+		imgr = null;
 		InputStream in = new ByteArrayInputStream(byteArray);
 		try {
-			img = ImageIO.read(in);
-			repaint();
+			imgr = ImageIO.read(in);
+			right.repaint();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		frame = new Mat();
+		capUSB.retrieve(frame);
+		//Highgui.imwrite("im1.jpg", frame);
+		tempMat = new MatOfByte();
+		Highgui.imencode(".jpg", frame, tempMat);
+		byteArray = tempMat.toArray();
+		imgl = null;
+		in = new ByteArrayInputStream(byteArray);
+		try {
+			imgl = ImageIO.read(in);
+			left.repaint();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
